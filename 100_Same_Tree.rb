536e3328bf6425a8be3463e52d7ed7bea12b8f75
Sriranjani <sriranjani48@gmail.com>
2018-07-20 08:58:51 -0700

    Solution to Same Tree problem
    Given two binary trees, write a function to check if they are the same or not.
    
    Two binary trees are considered the same if they are structurally identical and the nodes have the same value.

--- 100_Same_Tree.rb
+++ 100_Same_Tree.rb
@@ -1,21 +1,25 @@
-def same_tree(tree1, tree2)
-	i = 0
-	while i < tree2.length
-		if tree1[i] != tree2[i]
-			break
+class BinaryTree
+	def same_tree(tree1, tree2)
+		i = 0
+		while i < tree2.length
+			if tree1[i] != tree2[i]
+				break
+			end
+			i += 1
 		end
-		i += 1
-	end
-	if i == tree1.length
-		puts true 
-	else
-		puts false
+		if i == tree1.length
+			return true 
+		else
+			return false
+		end
 	end
 end
 
-same_tree([1,2,3], [1,2,3])
-same_tree([1,2], [1,nil,2])
-same_tree([1,2,1], [1,1,2])
-same_tree([1,1,3],[1,2,3])
-same_tree([1],[2])
-same_tree([1],[1])
+new_tree = BinaryTree.new
+new_tree.same_tree([1,2,3], [1,2,3])
+new_tree.same_tree([1,2], [1,nil,2])
+new_tree.same_tree([1,2,1], [1,1,2])
+
+#same_tree([1,1,3],[1,2,3])
+#same_tree([1],[2])
+#same_tree([1],[1])
---
+++ 100_Same_Tree_Test.rb
@@ -0,0 +1,11 @@
+require_relative "100_Same_Tree.rb"
+require "test/unit"
+
+class TestBinaryTreeSame < Test::Unit::TestCase
+	def test_given_two_same_trees_should_return_true
+		assert_equal(true, BinaryTree.new.same_tree([1,2,3], [1,2,3]))
+	end
+	def test_given_two_different_trees_should_return_false
+		assert_equal(false, BinaryTree.new.same_tree([1,2,1], [1,1,2]))
+	end
+end
