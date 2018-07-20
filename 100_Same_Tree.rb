fb3260a75507465af7387361cdec7682f0a63a24
Sriranjani <sriranjani48@gmail.com>
2018-07-20 08:39:36 -0700

    solution to same tree problem
    Given two binary trees, write a function to check if they are the same or not.
    
    Two binary trees are considered the same if they are structurally identical and the nodes have the same value.

--- 100_Same_Tree.rb
+++ 100_Same_Tree.rb
@@ -1,3 +1,21 @@
-def same_tree
-	puts "hello world"
+def same_tree(tree1, tree2)
+	i = 0
+	while i < tree2.length
+		if tree1[i] != tree2[i]
+			break
+		end
+		i += 1
+	end
+	if i == tree1.length
+		puts true 
+	else
+		puts false
+	end
 end
+
+same_tree([1,2,3], [1,2,3])
+same_tree([1,2], [1,nil,2])
+same_tree([1,2,1], [1,1,2])
+same_tree([1,1,3],[1,2,3])
+same_tree([1],[2])
+same_tree([1],[1])
